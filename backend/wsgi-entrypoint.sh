17fecdddd45ae339ae48d4bd8b20ed014e433b2e
Bramstorp <m.bramstorp@stofanet.dk>
2021-04-02 23:11:29 +0200

    fixed wsgi file failing + update to docker files in dev

--- backend/wsgi-entrypoint.sh
+++ backend/wsgi-entrypoint.sh
@@ -5,15 +5,15 @@ then
     echo "Waiting for postgres..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
-        sleep 0.1
+      sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
 
-python3 manage.py flush --no-input
-python3 manage.py migrate
-python3 manage.py collectstatic --noinput
+python manage.py flush --no-input
+python manage.py migrate
+python manage.py collectstatic --noinput
 
 exec "$@"
 
