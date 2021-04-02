647e11a606d3c08c7596e534d123b2810b1ec9b7
Bramstorp <m.bramstorp@stofanet.dk>
2021-04-02 19:40:13 +0200

    added dev docker files

---
+++ backend/wsgi-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python3 manage.py flush --no-input
+python3 manage.py migrate
+
+exec "$@"
+
+gunicorn base.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
