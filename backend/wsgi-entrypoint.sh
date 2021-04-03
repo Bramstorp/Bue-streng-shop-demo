a61f6caf71d84d5da1b36888201656efb31f928e
Bramstorp <m.bramstorp@stofanet.dk>
2021-04-03 19:54:49 +0200

    update

--- backend/wsgi-entrypoint.sh
+++ backend/wsgi-entrypoint.sh
@@ -17,4 +17,4 @@ python manage.py collectstatic --noinput
 
 exec "$@"
 
-gunicorn backend.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+gunicorn base.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
