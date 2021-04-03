fc5bf8603a12054aa18046d94f271c6141ba5831
Bramstorp <m.bramstorp@stofanet.dk>
2021-04-03 14:51:39 +0200

    fixed static

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/arrows/', include("arrows.urls")),
     path('api/strings/', include("strings.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
