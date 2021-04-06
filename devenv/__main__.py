eda77e567d221ccb6d1073bf30f8d07f9152c198
Bramstorp <m.bramstorp@stofanet.dk>
2021-04-06 19:30:46 +0200

    added docker commands for start and stop

--- devenv/__main__.py
+++ devenv/__main__.py
@@ -1,13 +1,19 @@
 import sys
+from .docker import docker
 
 
 def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
-
+    input_path = sys.argv[1]
+    if input_path == "docker":
+        env = sys.argv[2]
+        if env == "dev":
+            docker(env, sys.argv[3])
+        elif env == "prod":
+            docker(env, sys.argv[3])
+        else:
+            print("pls use dev or prod for env")
+    else:
+        print("fejl 40 dummert")
 
 if __name__ == '__main__':
     main()
