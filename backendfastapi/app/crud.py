e86e978d1a937d7b8589a851bd1f5849cb14f8dd
Mathias Bramstorp <m.bramstorp@stofanet.dk>
2021-06-25 19:54:38 +0200

    small frontend dockerfile update + working on fastapi demo

--- backendfastapi/app/crud.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from sqlalchemy.sql.functions import mode
-
-from . import models, schemas
-
-
-def get_arrows(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Arrows).offset(skip).limit(limit).all()
--- backendfastapi/app/main.py
+++ backendfastapi/app/main.py
@@ -1,31 +1,69 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+import databases
+import sqlalchemy
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+DATABASE_URL = "sqlite:///./test.db"
+# DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-models.Base.metadata.create_all(bind=engine)
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+notes = sqlalchemy.Table(
+    "notes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
+
+class NoteIn(BaseModel):
+    text: str
+    completed: bool
+
+
+class Note(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
 
 app = FastAPI()
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get("/")
-def root():
-    return {"Hello": "World"}
+async def root():
+    return "Test"
 
 
-@app.get("/arrows/", response_model=List[schemas.Arrows])
-def read_arrows(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    arrows = crud.get_arrows(db, skip=skip, limit=limit)
-    return arrows
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/notes/", response_model=Note)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), "id": last_record_id}
