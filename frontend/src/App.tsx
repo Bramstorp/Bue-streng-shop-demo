4c622f5bbea3c7783905baab104207e7ddc066a0
Mathias Bramstorp <m.bramstorp@stofanet.dk>
2021-07-05 20:51:23 +0200

    added not found page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { StringRoutes } from "./StringPages/StringRoutes";
 import { ArrowRoutes } from "./ArrowPages/ArrowRoutes";
 
-
 import { Home } from "./Main";
+import { NotFound } from "./NotFound"
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" exact={true} component={Home} />
         <Route path="/strings" exact={true} component={StringRoutes} />
         <Route path="/arrows" exact={true} component={ArrowRoutes} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
---
+++ frontend/src/NotFound.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+
+
+export const NotFound: FunctionComponent = () => {
+    return (
+        <h1>Not Found!</h1>
+    );
+  };
